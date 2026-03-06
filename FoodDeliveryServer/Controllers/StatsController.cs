7a5b6989ebe4c3563b6e9ec6c2ac2b735425309d
Heng <bigheadson1007@gmail.com>
2026-03-06 18:21:48 +0800

    Fix the architecture in StatsController

--- FoodDeliveryServer/Controllers/StatsController.cs
+++ FoodDeliveryServer/Controllers/StatsController.cs
@@ -1,5 +1,6 @@
 ﻿using FoodDeliveryServer.Data;
 using FoodDeliveryServer.Models;
+using FoodDeliveryServer.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,11 +10,11 @@ namespace FoodDeliveryServer.Controllers
     [ApiController]
     public class StatsController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        private IStatServices _statServices;
 
-        public StatsController(AppDbContext context)
+        public StatsController(IStatServices service)
         {
-            _context = context;
+            _statServices = service;
         }
 
         // GET: api/Stats/BestSellers
@@ -21,63 +22,22 @@ namespace FoodDeliveryServer.Controllers
         [HttpGet("BestSellers")]
         public async Task<ActionResult> GetBestSellers()
         {
-            var stats = await _context.OrderItems
-                // 1. 筛选：只要最近 7 天的订单
-                .Where(o => o.Order!.OrderDate >= DateTime.Now.AddDays(-7))
-
-                // 3. 分组：把相同名字的菜堆在一起 (比如所有的 Burger 放一堆)
-                .GroupBy(o => new { o.FoodId, o.Food!.Name })
-
-                // 4. 统计：对于每一堆(g)，我们要算出什么？
-                .Select(g => new
-                {
-                    FoodId = g.Key.FoodId,                       // 菜名
-                    FoodName = g.Key.Name,
-                    TotalSold = g.Sum(o => o.Quantity)  // 卖出的总数量 (把这一堆的 Quantity 加起来)
-                })
-
-                // 5. 排序：卖得最多的排在最上面 (Descending = 降序)
-                .OrderByDescending(x => x.TotalSold)
-
-                // 6. 截取：只取前 3 名
-                .Take(3)
-
-                // 7. 执行查询
-                .ToListAsync();
-
-            return Ok(stats);
+            var bestSellers = await _statServices.GetBestSellers();
+            return Ok(bestSellers);
         }
 
         [HttpGet("TopSpender")]
         public async Task<ActionResult> GetTopSpender()
         {
-            var stats = await _context.Orders
-                .Where(o => o.Status != OrderStatus.Cancelled)
-                .GroupBy(o => o.UserId)
-                .Select(g => new
-                {
-                    CustomerId = g.Key,
-                    TotalSpent = g.Sum(o => o.TotalPrice)
-                })
-                .OrderByDescending(x => x.TotalSpent)
-                .Take(1)
-                .ToListAsync();
-            return Ok(stats);
+            var topSpender = await _statServices.GetTopSpender();
+            return Ok(topSpender);
         }
 
         [HttpGet("Sentiment")]
         public async Task<ActionResult> GetSentimentStats()
         {
-            var stats = await _context.Orders
-                .GroupBy(o => o.Sentiment)
-                .Select(g => new
-                {
-                    Label = g.Key,
-                    Count = g.Count()
-                })
-                .ToListAsync();
-
-            return Ok(stats);
+            var sentimentStats = await _statServices.GetSentimentStats();
+            return Ok(sentimentStats);
         }
     }
 }
---
+++ FoodDeliveryServer/Services/IStatServices.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryServer.Services
+{
+    public interface IStatServices
+    {
+        Task<object> GetBestSellers();
+        Task<object> GetTopSpender();
+        Task<object> GetSentimentStats();
+    }
+}
